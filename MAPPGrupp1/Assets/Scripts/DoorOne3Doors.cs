6cd35ab17d7becbef8f39fcf87430e34a6db7527
Danzavodnik <131752902+Danzavodnik@users.noreply.github.com>
2023-04-26 18:09:05 +0200

    nya knapar
    nya knappar till dörr 1,2,3

---
+++ MAPPGrupp1/Assets/Scripts/DoorOne3Doors.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorOne3Doors : MonoBehaviour
+{
+    public void DoorOne()
+    {
+        print("error låst!");
+    }
+}
+
---
+++ MAPPGrupp1/Assets/Scripts/DoorTwo3Doors.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DoorTwo3Doors : MonoBehaviour
+{
+    public void DoorTwo()
+    {
+        SceneManager.LoadScene(11);
+    }
+}
