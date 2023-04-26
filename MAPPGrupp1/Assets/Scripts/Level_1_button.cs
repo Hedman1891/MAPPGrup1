7c1ae3bca6f8d1fd01e57e2b1f9cf55a7969cfc5
Danzavodnik <131752902+Danzavodnik@users.noreply.github.com>
2023-04-26 21:09:06 +0200

    knappar

---
+++ MAPPGrupp1/Assets/Scripts/Level_1_button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_1_button : MonoBehaviour
+{
+    public void Level_1()
+    {
+        SceneManager.LoadScene(6);
+    }
+}
---
+++ MAPPGrupp1/Assets/Scripts/Level_2_button.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level_2_button : MonoBehaviour
+{
+    public void Level2()
+    {
+        SceneManager.LoadScene(14);
+    }
+}
