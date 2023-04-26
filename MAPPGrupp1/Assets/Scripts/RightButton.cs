e83f985f2c509edbdc57b55830b9b09b7da05677
Danzavodnik <131752902+Danzavodnik@users.noreply.github.com>
2023-04-26 18:30:24 +0200

    ännu fler knappar
    knappar

---
+++ MAPPGrupp1/Assets/Scripts/RightButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RightButton : MonoBehaviour
+{
+    public void RightChoice()
+    {
+        SceneManager.LoadScene(12);
+    }
+}
