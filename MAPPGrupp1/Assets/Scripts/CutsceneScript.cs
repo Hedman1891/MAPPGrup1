b0d024cb38ceed0e336ebd4fe0685ade655d8cbc
Danzavodnik <131752902+Danzavodnik@users.noreply.github.com>
2023-05-04 15:08:39 +0200

    videos till scener och cutScene Script

---
+++ MAPPGrupp1/Assets/Scripts/CutsceneScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class CutsceneScript : MonoBehaviour
+{
+
+    public VideoPlayer videoPlayer;
+    public string nextSceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    // Update is called once per frame
+    void OnVideoEnd(VideoPlayer Player)
+    {
+        SceneManager.LoadScene(nextSceneName);
+    }
+}
