78dca6dd7629166a0a8bbac8d14fa71416353187
Danzavodnik <131752902+Danzavodnik@users.noreply.github.com>
2023-05-17 14:20:56 +0200

    ändringar

--- MAPPGrupp1/Assets/Scripts/NavigateScenes.cs
+++ MAPPGrupp1/Assets/Scripts/NavigateScenes.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class NavigateScenes : MonoBehaviour
-{
+{ 
     public void SwitchScene(string sceneName){
         SceneManager.LoadScene(sceneName);
     }
