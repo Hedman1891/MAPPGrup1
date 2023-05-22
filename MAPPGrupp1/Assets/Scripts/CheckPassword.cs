01a66d1f0b8968d153ef2f516a9785666f465172
Chuck Long Ching <131591076+NeonstarChuck@users.noreply.github.com>
2023-05-22 02:10:20 +0200

    Jag har gjort ett password script som leder till slut 1

---
+++ MAPPGrupp1/Assets/Scripts/CheckPassword.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class CheckPassword : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private InputField inputTextField;
+
+    void Start()
+    {
+        inputTextField = GetComponent<InputField>();
+    }
+
+    public void CheckPasswordAndLoadScene()
+    {
+
+    switch (inputTextField.text)
+
+    {
+        case "ESCAPE":
+            SceneManager.LoadScene("END_1");
+            break;
+        
+        default:
+        inputTextField.text = "";
+        break;
+    }
+
+    
+    
+}
+}
