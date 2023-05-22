c43c92b0b9bd7aacff688fe6b1c8e843d0fbf2f9
JenIsa-design <isacson.jennifer@gmail.com>
2023-05-22 17:08:33 +0200

    pling plong

--- MAPPGrupp1/Assets/Scripts/NavigateScenes.cs
+++ MAPPGrupp1/Assets/Scripts/NavigateScenes.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class NavigateScenes : MonoBehaviour
 { 
+    //skriv namnet på scenen som det ska bytas till i Unity när man lägger till scriptet på knappen
     public void SwitchScene(string sceneName){
         SceneManager.LoadScene(sceneName);
     }
