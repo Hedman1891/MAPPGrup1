62513c83c3167b38a0791bbee52df2e5a8c79c88
Chuck Long Ching <131591076+NeonstarChuck@users.noreply.github.com>
2023-05-27 16:35:26 +0200

    KodÄndring

--- MAPPGrupp1/Assets/Scripts/CheckPassword.cs
+++ MAPPGrupp1/Assets/Scripts/CheckPassword.cs
@@ -9,29 +9,32 @@ public class CheckPassword : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    private InputField inputTextField;
-
-    void Start()
-    {
-        inputTextField = GetComponent<InputField>();
-    }
+    public InputField inputTextField;
+    public Light[] pointLights;
 
     public void CheckPasswordAndLoadScene()
     {
+        switch (inputTextField.text)
+        {
+            case "ESCAPE":
+                SceneManager.LoadScene("Garage_Correct_Ending");
+                break;
 
-    switch (inputTextField.text)
-
-    {
-        case "ESCAPE":
-            SceneManager.LoadScene("Garage_Correct_Ending");
-            break;
-        
-        default:
-        inputTextField.text = "Wrong Password";
-        break;
+            default:
+                inputTextField.text = "Wrong Password";
+                DisablePointLights();
+                break;
+        }
     }
 
-    
-    
-}
+    private void DisablePointLights()
+    {
+        foreach (Light pointLight in pointLights)
+        {
+            if (pointLight != null)
+            {
+                pointLight.enabled = false;
+            }
+        }
+    }
 }
