81c4478475189ae87cb334df72d645aba1882c21
Chuck Long Ching <131591076+NeonstarChuck@users.noreply.github.com>
2023-05-28 20:42:56 +0200

    DÖRR1 Borta efter click script

--- MAPPGrupp1/Assets/Scripts/ClickDestroy.cs
+++ MAPPGrupp1/Assets/Scripts/ClickDestroy.cs
@@ -2,13 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PopupLocke : MonoBehaviour
+public class ClickDestroy : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public void DestroyThisObject()
+{
+   Destroy(gameObject);
+}
 
     // Update is called once per frame
     void Update()
